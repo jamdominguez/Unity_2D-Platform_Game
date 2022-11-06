8f895f7252f5985e3aa1b099829e45757677a9f9
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2022-11-06 02:05:33 +0100

    fruit prefabs added

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -4,22 +4,15 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player") {
-            Debug.Log("COLLECTED!");
             GetComponent<Animator>().SetTrigger("collected");
+            Debug.Log("collected:" + gameObject.name);
         }
-        
     }
 
     public void DestroyFruit() {
-        Debug.Log("DESTROY!");
         Destroy(gameObject);
     }
 }
